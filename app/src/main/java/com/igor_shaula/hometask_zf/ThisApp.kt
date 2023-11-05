d02f9d7fd42d7422dbd0a931b9e4487fadc65d47
Igor Shaula <igor.shaula@gmail.com>
2023-11-06 00:11:30 +0200

    + renamed Application class & linked it into Manifest

--- app/src/main/java/com/igor_shaula/hometask_zf/ThisApp.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/ThisApp.kt
@@ -3,7 +3,7 @@ package com.igor_shaula.hometask_zf
 import android.app.Application
 import timber.log.Timber
 
-class CustomApplication : Application() {
+class ThisApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
