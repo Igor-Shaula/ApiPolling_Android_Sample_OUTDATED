90a2bb14f01e2f796e55bc4a41af46b2218ae36a
Igor Shaula <igor.shaula@gmail.com>
2023-11-06 14:13:26 +0200

    wrapped readTheData() invocation into coroutine scope

--- app/src/main/java/com/igor_shaula/hometask_zf/CarListViewModel.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/CarListViewModel.kt
@@ -2,6 +2,9 @@ package com.igor_shaula.hometask_zf
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class CarListViewModel : ViewModel() {
 
@@ -10,6 +13,9 @@ class CarListViewModel : ViewModel() {
     private var repository: CarsRepository = CarsRepository()
 
     init {
-        carListData.value = repository.readTheData()
+        MainScope().launch {
+            carListData.value = repository.readTheData()
+            Timber.d("carListData = ${carListData.value}")
+        }
     }
 }
