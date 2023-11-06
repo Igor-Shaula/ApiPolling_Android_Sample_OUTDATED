7c43d12e8c4961627c86b8546619e8205dff3442
Igor Shaula <igor.shaula@gmail.com>
2023-11-06 02:33:04 +0200

    + basic content into CarListViewModel

--- app/src/main/java/com/igor_shaula/hometask_zf/CarListViewModel.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/CarListViewModel.kt
@@ -1,7 +1,15 @@
 package com.igor_shaula.hometask_zf
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CarListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val carListData = MutableLiveData<List<CarItemRecord>>()
+
+    private var repository: CarsRepository = CarsRepository()
+
+    init {
+        carListData.value = repository.readTheData()
+    }
 }
