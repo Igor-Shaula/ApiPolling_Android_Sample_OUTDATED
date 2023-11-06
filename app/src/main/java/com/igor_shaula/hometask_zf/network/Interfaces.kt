7cd48da50ae1d0cecea352fe0097cb378819e88b
Igor Shaula <igor.shaula@gmail.com>
2023-11-06 14:02:34 +0200

    used protected api-key in the request's header

--- app/src/main/java/com/igor_shaula/hometask_zf/network/Interfaces.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/network/Interfaces.kt
@@ -1,11 +1,12 @@
 package com.igor_shaula.hometask_zf.network
 
+import com.igor_shaula.hometask_zf.BuildConfig
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
 
 interface CarDataNetworksService {
-    @Headers("$API_KEY_HEADER: secret must not be exposed to the open public")
+    @Headers("$API_KEY_HEADER: ${BuildConfig.API_KEY}")
     @GET(API_ENDPOINT_VEHICLES)
     suspend fun getCarList(): Response<List<CarModel>>
 }
