3f4d66104672140e42f3e910cd00cd3317b35242
Igor Shaula <igor.shaula@gmail.com>
2023-11-07 03:13:27 +0200

    + VehicleDetailsModel & LocationModel

--- app/src/main/java/com/igor_shaula/hometask_zf/network/Models.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/network/Models.kt
@@ -3,3 +3,13 @@ package com.igor_shaula.hometask_zf.network
 import com.google.gson.annotations.SerializedName
 
 data class VehicleModel(@SerializedName(SERIALIZATION_KEY_VEHICLE_ID) val vehicleId: String)
+
+data class VehicleDetailsModel(
+    @SerializedName(SERIALIZATION_KEY_VEHICLE_ID) val vehicleId: String,
+    @SerializedName(SERIALIZATION_KEY_LOCATION) val location: LocationModel
+)
+
+data class LocationModel(
+    @SerializedName(SERIALIZATION_KEY_LATITUDE) val latitude: Double,
+    @SerializedName(SERIALIZATION_KEY_LONGITUDE) val longitude: Double
+)
