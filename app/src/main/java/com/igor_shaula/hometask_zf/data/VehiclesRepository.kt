352a38cbf1f77408c60c55017850296cf0b2fc00
Igor Shaula <igor.shaula@gmail.com>
2023-11-07 03:20:39 +0200

    + readVehicleDetails() in the repository

--- app/src/main/java/com/igor_shaula/hometask_zf/data/VehiclesRepository.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/data/VehiclesRepository.kt
@@ -1,7 +1,8 @@
 package com.igor_shaula.hometask_zf.data
 
-import com.igor_shaula.hometask_zf.network.VehicleNetworkServiceImpl
+import com.igor_shaula.hometask_zf.network.VehicleDetailsModel
 import com.igor_shaula.hometask_zf.network.VehicleModel
+import com.igor_shaula.hometask_zf.network.VehicleNetworkServiceImpl
 
 class VehiclesRepository {
 
@@ -9,6 +10,12 @@ class VehiclesRepository {
         val vehicleDataNetworkService = VehicleNetworkServiceImpl()
         val vehicleList = vehicleDataNetworkService.getVehiclesList()
         return vehicleList.body().toVehicleItemRecords()
+    }
+
+    suspend fun readVehicleDetails(vehicleId: String): VehicleDetailsRecord? {
+        val vehicleDataNetworkService = VehicleNetworkServiceImpl()
+        val vehicleDetails = vehicleDataNetworkService.getVehicleDetails(vehicleId)
+        return vehicleDetails.body()?.toVehicleItemRecords()
     }
 }
 
@@ -19,3 +26,10 @@ private fun List<VehicleModel>?.toVehicleItemRecords(): List<VehicleRecord> {
     }
     return result
 }
+
+private fun VehicleDetailsModel.toVehicleItemRecords(): VehicleDetailsRecord =
+    VehicleDetailsRecord(
+        vehicleId = this.vehicleId,
+        latitude = this.location.latitude,
+        longitude = this.location.longitude
+    )
