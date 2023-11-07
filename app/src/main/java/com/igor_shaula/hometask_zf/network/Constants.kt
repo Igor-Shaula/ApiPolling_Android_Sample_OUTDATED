ec2a5de3e8a33c8ea74a1a945b83509ae4863936
Igor Shaula <igor.shaula@gmail.com>
2023-11-07 03:12:38 +0200

    slightly modified Constants.kt

--- app/src/main/java/com/igor_shaula/hometask_zf/network/Constants.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/network/Constants.kt
@@ -5,7 +5,8 @@ const val API_BASE_URL = "https://5w53f1x8oa.execute-api.eu-west-1.amazonaws.com
 const val API_KEY_HEADER = "x-api-key"
 
 const val API_ENDPOINT_VEHICLES_LIST = "vehicles"
-const val API_ENDPOINT_VEHICLE_DETAILS = "vehicles/:vehicleId"
+const val API_ENDPOINT_VEHICLE_DETAILS_BASE = "vehicles"
+const val API_PATH_VEHICLE_ID = "vehicleId"
 
 const val SERIALIZATION_KEY_VEHICLE_ID = "vehicleId"
 const val SERIALIZATION_KEY_LOCATION = "location"
