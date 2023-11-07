fbc42dc62893f716014736f628d88aceb16a21b3
Igor Shaula <igor.shaula@gmail.com>
2023-11-07 02:10:26 +0200

    + new constants for another network request

--- app/src/main/java/com/igor_shaula/hometask_zf/network/Constants.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/network/Constants.kt
@@ -5,5 +5,9 @@ const val API_BASE_URL = "https://5w53f1x8oa.execute-api.eu-west-1.amazonaws.com
 const val API_KEY_HEADER = "x-api-key"
 
 const val API_ENDPOINT_VEHICLES_LIST = "vehicles"
+const val API_ENDPOINT_VEHICLE_DETAILS = "vehicles/:vehicleId"
 
-const val SERIALIZATION_KEY_VEHICLE = "vehicleId"
+const val SERIALIZATION_KEY_VEHICLE_ID = "vehicleId"
+const val SERIALIZATION_KEY_LOCATION = "location"
+const val SERIALIZATION_KEY_LATITUDE = "latitude"
+const val SERIALIZATION_KEY_LONGITUDE = "longitude"
