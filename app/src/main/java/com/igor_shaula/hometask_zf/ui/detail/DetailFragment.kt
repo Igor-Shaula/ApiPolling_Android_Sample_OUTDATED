e1dd8cadccd2d9302debd2ac64d48380fb6284f7
Igor Shaula <igor.shaula@gmail.com>
2023-11-08 13:11:09 +0200

    slightly adjusted DialogFragment content

--- app/src/main/java/com/igor_shaula/hometask_zf/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/ui/detail/DetailFragment.kt
@@ -5,27 +5,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
-import com.igor_shaula.hometask_zf.R
 import com.igor_shaula.hometask_zf.databinding.FragmentDetailBinding
 
-class DetailFragment : Fragment() {
+class DetailFragment : DialogFragment() {
 
     private lateinit var binding: FragmentDetailBinding
 
     private lateinit var viewModel: DetailViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel = ViewModelProvider(this)[DetailViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
