1e6cd82fc50b3b8a5ae12d142dac69b7b50388e1
Igor Shaula <igor.shaula@gmail.com>
2023-11-08 15:52:18 +0200

    + content in the DetailViewModel

--- app/src/main/java/com/igor_shaula/hometask_zf/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/igor_shaula/hometask_zf/ui/detail/DetailViewModel.kt
@@ -1,7 +1,14 @@
 package com.igor_shaula.hometask_zf.ui.detail
 
 import androidx.lifecycle.ViewModel
+import com.igor_shaula.hometask_zf.ThisApp
+import com.igor_shaula.hometask_zf.data.VehiclesRepository
 
 class DetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var repository: VehiclesRepository = ThisApp.getVehiclesRepository()
+
+    val vehiclesDetailsMap = repository.vehicleDetailsMapMLD
+
+    val vehiclesMap = repository.vehiclesMapMLD
 }
