8ac49fd26fa47936b3cb957133f51cf21ec49019
igor.shaula <igor.shaula@gmail.com>
2023-12-20 22:42:44 +0200

    protected the app from falling with Uncaught & unknown exceptions

--- app/src/main/kotlin/com/igor_shaula/api_polling/ui_layer/MainActivity.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/ui_layer/MainActivity.kt
@@ -3,11 +3,16 @@ package com.igor_shaula.api_polling.ui_layer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.igor_shaula.api_polling.R
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, exception ->
+            Timber.e("caught exception: $exception\n\ton thread $thread")
+        }
     }
 }
