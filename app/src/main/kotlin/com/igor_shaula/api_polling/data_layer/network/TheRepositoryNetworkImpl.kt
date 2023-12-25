989e022f46e19abf76d97b63d47cb99e2d5a3a1a
igor.shaula <igor.shaula@gmail.com>
2023-12-26 01:58:18 +0200

    cleaned TheRepositoryNetworkImpl from all code which is now in AbstractRepository

--- app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/network/TheRepositoryNetworkImpl.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/network/TheRepositoryNetworkImpl.kt
@@ -1,61 +1,15 @@
 package com.igor_shaula.api_polling.data_layer.network
 
-import com.igor_shaula.api_polling.data_layer.DEFAULT_POLLING_INTERVAL
-import com.igor_shaula.api_polling.data_layer.JavaTPEBasedPollingEngine
-import com.igor_shaula.api_polling.data_layer.PollingEngine
-import com.igor_shaula.api_polling.data_layer.TheRepository
+import com.igor_shaula.api_polling.data_layer.AbstractRepository
 import com.igor_shaula.api_polling.data_layer.VehicleDetailsRecord
 import com.igor_shaula.api_polling.data_layer.VehicleRecord
-import com.igor_shaula.api_polling.data_layer.VehicleStatus
-import com.igor_shaula.api_polling.data_layer.detectNumberOfNearVehicles
 import com.igor_shaula.api_polling.data_layer.network.retrofit.VehicleNetworkServiceImpl
 import com.igor_shaula.api_polling.data_layer.toVehicleItemRecords
-import com.igor_shaula.api_polling.data_layer.toVehicleRecordList
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.plus
 import timber.log.Timber
 
-class TheRepositoryNetworkImpl : TheRepository {
+class TheRepositoryNetworkImpl : AbstractRepository() {
 
-    private var pollingEngine: PollingEngine? = null
-
-    private val coroutineScope = MainScope() + CoroutineName(this.javaClass.simpleName)
-
-    override suspend fun getAllVehiclesIds(): MutableMap<String, VehicleStatus> =
-        readVehiclesList()
-            .associateBy({ it.vehicleId }, { it.vehicleStatus })
-            .toMutableMap()
-
-    override suspend fun startGettingVehiclesDetails(
-        vehiclesMap: MutableMap<String, VehicleStatus>?,
-        updateViewModel: (Pair<String, VehicleDetailsRecord>) -> Unit
-    ) {
-        vehiclesMap?.let {
-            preparePollingEngine(it.size)
-            vehiclesMap.forEach { (key, _) ->
-                pollingEngine?.launch(DEFAULT_POLLING_INTERVAL) {
-                    coroutineScope.launch {
-                        getAllDetailsForOneVehicle(key, updateViewModel)
-                    }
-                }
-            }
-        }
-    }
-
-    override fun stopGettingVehiclesDetails() {
-        pollingEngine?.stopAndClearItself()
-        coroutineScope.cancel()
-    }
-
-    override fun getNumberOfNearVehicles(vehiclesMap: MutableMap<String, VehicleStatus>?): Int {
-        val vehicleRecordsList = vehiclesMap?.toList()?.toVehicleRecordList()
-        return detectNumberOfNearVehicles(vehicleRecordsList)
-    }
-
-    private suspend fun readVehiclesList(): List<VehicleRecord> {
+    override suspend fun readVehiclesList(): List<VehicleRecord> {
         val vehicleDataNetworkService = VehicleNetworkServiceImpl()
         Timber.v("readVehiclesList: created vehicleDataNetworkService: ${vehicleDataNetworkService.hashCode()}")
         val vehicleList = vehicleDataNetworkService.getVehiclesList()
@@ -63,22 +17,7 @@ class TheRepositoryNetworkImpl : TheRepository {
         return vehicleList.body().toVehicleItemRecords()
     }
 
-    private fun preparePollingEngine(size: Int) {
-//        if (pollingEngine == null)
-        pollingEngine = JavaTPEBasedPollingEngine.prepare(size)
-    }
-
-    private suspend fun getAllDetailsForOneVehicle(
-        vehicleId: String, updateViewModel: (Pair<String, VehicleDetailsRecord>) -> Unit
-    ) {
-        val vehicleDetails = readVehicleDetails(vehicleId)
-        Timber.d("vehicleDetails = $vehicleDetails")
-        if (vehicleDetails != null) {
-            updateViewModel(Pair(vehicleId, vehicleDetails))
-        }
-    }
-
-    private suspend fun readVehicleDetails(vehicleId: String): VehicleDetailsRecord? {
+    override suspend fun readVehicleDetails(vehicleId: String): VehicleDetailsRecord? {
         val vehicleDataNetworkService = VehicleNetworkServiceImpl()
         val vehicleDetails = vehicleDataNetworkService.getVehicleDetails(vehicleId)
         return vehicleDetails.body()?.toVehicleItemRecords()
