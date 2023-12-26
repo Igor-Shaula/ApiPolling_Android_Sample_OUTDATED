fd4502e9a921056560889ca5becc0e1bdd8e1af5
igor.shaula <igor.shaula@gmail.com>
2023-12-26 02:01:19 +0200

    optimized & simplified use of vehicleDataNetworkService in TheRepositoryNetworkImpl

--- app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/network/TheRepositoryNetworkImpl.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/network/TheRepositoryNetworkImpl.kt
@@ -5,21 +5,14 @@ import com.igor_shaula.api_polling.data_layer.VehicleDetailsRecord
 import com.igor_shaula.api_polling.data_layer.VehicleRecord
 import com.igor_shaula.api_polling.data_layer.network.retrofit.VehicleNetworkServiceImpl
 import com.igor_shaula.api_polling.data_layer.toVehicleItemRecords
-import timber.log.Timber
 
 class TheRepositoryNetworkImpl : AbstractRepository() {
 
-    override suspend fun readVehiclesList(): List<VehicleRecord> {
-        val vehicleDataNetworkService = VehicleNetworkServiceImpl()
-        Timber.v("readVehiclesList: created vehicleDataNetworkService: ${vehicleDataNetworkService.hashCode()}")
-        val vehicleList = vehicleDataNetworkService.getVehiclesList()
-        Timber.v("readVehiclesList: received vehicleList: $vehicleList")
-        return vehicleList.body().toVehicleItemRecords()
-    }
+    private val vehicleDataNetworkService = VehicleNetworkServiceImpl()
 
-    override suspend fun readVehicleDetails(vehicleId: String): VehicleDetailsRecord? {
-        val vehicleDataNetworkService = VehicleNetworkServiceImpl()
-        val vehicleDetails = vehicleDataNetworkService.getVehicleDetails(vehicleId)
-        return vehicleDetails.body()?.toVehicleItemRecords()
-    }
+    override suspend fun readVehiclesList(): List<VehicleRecord> =
+        vehicleDataNetworkService.getVehiclesList().body().toVehicleItemRecords()
+
+    override suspend fun readVehicleDetails(vehicleId: String): VehicleDetailsRecord? =
+        vehicleDataNetworkService.getVehicleDetails(vehicleId).body()?.toVehicleItemRecords()
 }
