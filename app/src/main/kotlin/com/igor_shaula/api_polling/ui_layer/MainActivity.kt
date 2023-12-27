aebbf068f2070b6be35c5adce74173f86b5d0b5a
igor.shaula <igor.shaula@gmail.com>
2023-12-27 23:41:53 +0200

    + createAlertDialogForProvidingWithStubData() for the AlertDialog preparation

--- app/src/main/kotlin/com/igor_shaula/api_polling/ui_layer/MainActivity.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/ui_layer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.igor_shaula.api_polling.ui_layer
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.igor_shaula.api_polling.R
 import timber.log.Timber
@@ -15,4 +16,16 @@ class MainActivity : AppCompatActivity() {
             Timber.e("caught exception: $exception\n\ton thread $thread")
         }
     }
+
+    fun createAlertDialogForProvidingWithStubData(): AlertDialog =
+        AlertDialog.Builder(this)
+            .setTitle("there is good stub data...")
+            .setMessage("switch to using stub data?")
+            .setPositiveButton("Positive") { _, _ ->
+                Timber.v("positive button clicked")
+            }
+            .setNegativeButton("Negative") { _, _ ->
+                Timber.v("negative button clicked")
+            }
+            .create()
 }
