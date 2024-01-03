35f99ab64d8ac845455734294d479eb2f177f503
igor.shaula <igor.shaula@gmail.com>
2024-01-03 23:35:40 +0200

    + VehicleStubNetworkServiceImpl & used it instead of Retrofit - to just check

--- app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/network/NetworkRepositoryImpl.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/network/NetworkRepositoryImpl.kt
@@ -3,12 +3,13 @@ package com.igor_shaula.api_polling.data_layer.network
 import com.igor_shaula.api_polling.data_layer.AbstractVehiclesRepository
 import com.igor_shaula.api_polling.data_layer.VehicleDetailsRecord
 import com.igor_shaula.api_polling.data_layer.VehicleRecord
-import com.igor_shaula.api_polling.data_layer.network.retrofit.VehicleRetrofitNetworkServiceImpl
+import com.igor_shaula.api_polling.data_layer.network.api_stub.VehicleStubNetworkServiceImpl
 import com.igor_shaula.api_polling.data_layer.toVehicleItemRecords
 
 class NetworkRepositoryImpl : AbstractVehiclesRepository() {
 
-    private val vehicleDataNetworkService = VehicleRetrofitNetworkServiceImpl()
+    //    private val vehicleDataNetworkService = VehicleRetrofitNetworkServiceImpl()
+    private val vehicleDataNetworkService = VehicleStubNetworkServiceImpl()
 
     override suspend fun readVehiclesList(): List<VehicleRecord> =
         vehicleDataNetworkService.getVehiclesList().toVehicleItemRecords()
---
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/network/api_stub/VehicleStubNetworkServiceImpl.kt
@@ -0,0 +1,38 @@
+package com.igor_shaula.api_polling.data_layer.network.api_stub
+
+import com.igor_shaula.api_polling.data_layer.TARGET_LATITUDE
+import com.igor_shaula.api_polling.data_layer.TARGET_LONGITUDE
+import com.igor_shaula.api_polling.data_layer.network.LocationModel
+import com.igor_shaula.api_polling.data_layer.network.VehicleDetailsModel
+import com.igor_shaula.api_polling.data_layer.network.VehicleModel
+import kotlinx.coroutines.delay
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+class VehicleStubNetworkServiceImpl {
+
+    suspend fun getVehiclesList(): List<VehicleModel> {
+        delay(Random.nextInt(50..5_000).toLong())
+        val result: MutableList<VehicleModel> = mutableListOf()
+        result.add(VehicleModel("API stub #1"))
+        result.add(VehicleModel("API stub #2"))
+        result.add(VehicleModel("API stub #3"))
+        result.add(VehicleModel("API stub #4"))
+        result.add(VehicleModel("API stub #5"))
+        result.add(VehicleModel("API stub #6"))
+        result.add(VehicleModel("API stub #7"))
+        result.add(VehicleModel("API stub #8"))
+        result.add(VehicleModel("API stub #9"))
+        return result
+    }
+
+    suspend fun getVehicleDetails(vehicleId: String): VehicleDetailsModel? {
+        val randomCoefficient = if (Random.nextBoolean()) 0.001 else -0.001
+        val randomShift = randomCoefficient * Random.nextInt(0..10)
+        val newLocationModel = LocationModel(
+            TARGET_LATITUDE + randomShift, TARGET_LONGITUDE + randomShift
+        )
+        delay(Random.nextInt(10..10_000).toLong())
+        return VehicleDetailsModel(vehicleId, newLocationModel)
+    }
+}
