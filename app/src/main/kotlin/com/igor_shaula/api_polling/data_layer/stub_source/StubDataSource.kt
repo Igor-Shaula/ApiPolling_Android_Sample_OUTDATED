8ac87e52f732428a25a2f7a7e61e7e91e3f77b27
igor.shaula <igor.shaula@gmail.com>
2024-01-19 02:28:45 +0200

    adjusted getting stub data from getVehiclesList() with random upper limit

--- app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/stub_source/StubDataSource.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/stub_source/StubDataSource.kt
@@ -5,6 +5,7 @@ import com.igor_shaula.api_polling.data_layer.TARGET_LONGITUDE
 import com.igor_shaula.api_polling.data_layer.network.LocationModel
 import com.igor_shaula.api_polling.data_layer.network.VehicleDetailsModel
 import com.igor_shaula.api_polling.data_layer.network.VehicleModel
+import com.igor_shaula.stub_data_generator.StubVehicleGenerator
 import kotlinx.coroutines.delay
 import kotlin.random.Random
 import kotlin.random.nextInt
@@ -13,16 +14,11 @@ class StubDataSource {
 
     suspend fun getVehiclesList(): List<VehicleModel> {
         delay(Random.nextInt(50..5_000).toLong())
+        val generator = StubVehicleGenerator("stub vehicle #")
         val result: MutableList<VehicleModel> = mutableListOf()
-        result.add(VehicleModel("#1"))
-        result.add(VehicleModel("#2"))
-        result.add(VehicleModel("#3"))
-        result.add(VehicleModel("#4"))
-        result.add(VehicleModel("#5"))
-        result.add(VehicleModel("#6"))
-        result.add(VehicleModel("#7"))
-        result.add(VehicleModel("#8"))
-        result.add(VehicleModel("#9"))
+        (0..Random.nextInt(100)).forEach { _ ->
+            result.add(VehicleModel(generator.createNextVehicleModelString()))
+        }
         return result
     }
 
