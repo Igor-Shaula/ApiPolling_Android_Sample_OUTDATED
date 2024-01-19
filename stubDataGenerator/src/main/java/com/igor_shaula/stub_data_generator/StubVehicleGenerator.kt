2c83c8230f9b9356e04ee3fc518a515f9080407a
igor.shaula <igor.shaula@gmail.com>
2024-01-19 02:23:18 +0200

    adjusted numbering inside StubVehicleGenerator

--- stubDataGenerator/src/main/java/com/igor_shaula/stub_data_generator/StubVehicleGenerator.kt
+++ stubDataGenerator/src/main/java/com/igor_shaula/stub_data_generator/StubVehicleGenerator.kt
@@ -2,11 +2,9 @@ package com.igor_shaula.stub_data_generator
 
 // TODO this class should produce data similar to what is coming over network - in String/JSON type
 
-class StubVehicleGenerator(
-    private val vehicleIdBase: String,
-//    private val neededQuantity: Int
-) {
+class StubVehicleGenerator(private val vehicleIdBase: String) {
+
     private var currentVehicleIndex = 0
 
-    fun createNextVehicleModelString() = vehicleIdBase + currentVehicleIndex++
+    fun createNextVehicleModelString() = vehicleIdBase + (++currentVehicleIndex)
 }
