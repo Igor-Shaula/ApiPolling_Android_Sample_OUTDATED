e28431ad89d66a8159c5b3ca9ad6b3f8b263ec27
igor.shaula <igor.shaula@gmail.com>
2024-01-20 03:19:17 +0200

    + enableStrictMode() in the App class

--- app/src/main/kotlin/com/igor_shaula/api_polling/ThisApp.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/ThisApp.kt
@@ -2,6 +2,9 @@ package com.igor_shaula.api_polling
 
 import android.app.Application
 import android.content.Context
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
@@ -21,10 +24,31 @@ private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(na
 class ThisApp : Application() {
 
     override fun onCreate() {
+        enableStrictMode()
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+    }
+
+    // why we need this - https://developer.android.com/reference/android/os/StrictMode.html
+    private fun enableStrictMode() {
+        StrictMode.setThreadPolicy(
+            ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork() // or .detectAll() for all detectable problems
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
     }
 
     fun readNeedStubDialogFromLocalPrefs(): Flow<Boolean> =
