1d7553b25117842d306a5b170c71757afce9d3cf
igor.shaula <igor.shaula@gmail.com>
2024-01-24 05:06:27 +0200

    commented out unused read-write DataStore code in the App class

--- app/src/main/kotlin/com/igor_shaula/api_polling/ThisApp.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/ThisApp.kt
@@ -1,24 +1,16 @@
 package com.igor_shaula.api_polling
 
 import android.app.Application
-import android.content.Context
 import android.os.StrictMode
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.preferencesDataStore
 import com.igor_shaula.api_polling.data_layer.VehiclesRepository
 import com.igor_shaula.api_polling.data_layer.data_sources.NetworkDataSource
 import com.igor_shaula.api_polling.data_layer.data_sources.StubDataSource
 import com.igor_shaula.api_polling.data_layer.data_sources.retrofit.VehicleRetrofitNetworkServiceImpl
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import timber.log.Timber
 
-val TIME_TO_SHOW_GOTO_STUB_DIALOG = booleanPreferencesKey("timeToShowGoToStubDialog")
+//val TIME_TO_SHOW_GOTO_STUB_DIALOG = booleanPreferencesKey("timeToShowGoToStubDialog")
 
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "localDataStore")
+//private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "localDataStore")
 
 class ThisApp : Application() {
 
@@ -30,16 +22,16 @@ class ThisApp : Application() {
         }
     }
 
-    fun readNeedStubDialogFromLocalPrefs(): Flow<Boolean> =
-        dataStore.data.map { preferences ->
-            preferences[TIME_TO_SHOW_GOTO_STUB_DIALOG] ?: false
-        }
+//    fun readNeedStubDialogFromLocalPrefs(): Flow<Boolean> =
+//        dataStore.data.map { preferences ->
+//            preferences[TIME_TO_SHOW_GOTO_STUB_DIALOG] ?: false
+//        }
 
-    suspend fun saveNeedStubDialogToLocalPrefs(showStubDataNextTime: Boolean) {
-        dataStore.edit { preferences ->
-            preferences[TIME_TO_SHOW_GOTO_STUB_DIALOG] = showStubDataNextTime
-        }
-    }
+//    suspend fun saveNeedStubDialogToLocalPrefs(showStubDataNextTime: Boolean) {
+//        dataStore.edit { preferences ->
+//            preferences[TIME_TO_SHOW_GOTO_STUB_DIALOG] = showStubDataNextTime
+//        }
+//    }
 
     enum class ActiveDataSource {
         NETWORK, STUB
