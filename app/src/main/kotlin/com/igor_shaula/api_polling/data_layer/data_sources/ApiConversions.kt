122b4d2347de2119b98afbe9e7cd4cf9dd6575da
igor.shaula <igor.shaula@gmail.com>
2024-01-28 02:01:05 +0200

    + readVehiclesListResult() conversion

--- app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/data_sources/ApiConversions.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/data_sources/ApiConversions.kt
@@ -12,9 +12,28 @@ fun List<VehicleModel>?.toVehicleItemRecords(): List<VehicleRecord> {
     return result
 }
 
+fun Result<List<VehicleModel>>.toVehicleItemRecordsResult(): Result<List<VehicleRecord>> =
+    if (this.isFailure) {
+        Result.failure(
+            this.exceptionOrNull() ?: Throwable(message = ABSENT_FAILURE_INSTANCE_MESSAGE)
+        )
+    } else {
+        val resultList = mutableListOf<VehicleRecord>()
+        this.getOrNull()?.forEach {
+            resultList.add(
+                VehicleRecord(vehicleId = it.vehicleId, vehicleStatus = VehicleStatus.UNKNOWN)
+            )
+        }
+        // if incoming list was empty (valid case) - a new empty list is emitted - there is no error with it
+        Result.success(resultList)
+        // here i don't emit Result.failure instance for nullable case - because in fact it cannot happen
+    }
+
 fun VehicleDetailsModel.toVehicleItemRecords(): VehicleDetailsRecord =
     VehicleDetailsRecord(
         vehicleId = this.vehicleId,
         latitude = this.location.latitude,
         longitude = this.location.longitude
     )
+
+private const val ABSENT_FAILURE_INSTANCE_MESSAGE = "absent failure instance in Network API Result"
--- app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/data_sources/NetworkDataSource.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/data_sources/NetworkDataSource.kt
@@ -4,13 +4,17 @@ import com.igor_shaula.api_polling.data_layer.VehicleDetailsRecord
 import com.igor_shaula.api_polling.data_layer.VehicleRecord
 import com.igor_shaula.api_polling.data_layer.data_sources.retrofit.VehicleRetrofitNetworkServiceImpl
 
-// potentially this class can consume several different API clients - not only Retrofit
+// this class should consume several different API clients - not only Retrofit
 class NetworkDataSource(
     private val vehicleDataNetworkService: VehicleRetrofitNetworkServiceImpl
+    // add other network API clients as dependencies here
 ) {
 
     suspend fun readVehiclesList(): List<VehicleRecord> =
         vehicleDataNetworkService.getVehiclesList().toVehicleItemRecords()
+
+    suspend fun readVehiclesListResult(): Result<List<VehicleRecord>> =
+        vehicleDataNetworkService.getVehiclesListResult().toVehicleItemRecordsResult()
 
     suspend fun readVehicleDetails(vehicleId: String): VehicleDetailsRecord? =
         vehicleDataNetworkService.getVehicleDetails(vehicleId)?.toVehicleItemRecords()
