911653ff45c5af30145a6a3d9bac393d7ff29d85
igor.shaula <igor.shaula@gmail.com>
2024-01-29 02:59:34 +0200

    fixed typeface of dynamic animated text in AnimatedStringProgress

--- utilities/src/main/kotlin/com/igor_shaula/utilities/AnimatedStringProgress.kt
+++ utilities/src/main/kotlin/com/igor_shaula/utilities/AnimatedStringProgress.kt
@@ -41,7 +41,7 @@ class AnimatedStringProgress(private val textView: TextView) {
         textTypefaceBeforeAnimation = textView.typeface
         val textLength = textView.text.length
         val animationFrameTime = TOTAL_ANIMATION_TIME / textLength
-        textView.setTypeface(textView.typeface, Typeface.BOLD)
+        textView.setTypeface(Typeface.MONOSPACE, Typeface.BOLD)
         textView.setTextColor(Color.MAGENTA)
 //        textDotsCoroutineScope.launch { // text is not updated for unknown reason
         textAnimationJob = (GlobalScope + CoroutineName(textView.text.toString()))
