4368f1550d4487784aeb3e2c6e276cc58280b461
igor.shaula <igor.shaula@gmail.com>
2024-01-29 21:11:05 +0200

    + CurrentLocation data class & included its default value in VehicleRecord

--- app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/CommonModels.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/CommonModels.kt
@@ -3,7 +3,13 @@ package com.igor_shaula.api_polling.data_layer
 data class VehicleRecord(
     val vehicleId: String,
     val vehicleStatus: VehicleStatus,
-    val isBusyGettingNewData: Boolean = false
+    val isBusyGettingNewData: Boolean = false,
+    val currentLocation: CurrentLocation? = null
+)
+
+data class CurrentLocation(
+    val latitude: Double,
+    val longitude: Double
 )
 
 enum class VehicleStatus(val uiStatus: String, val isNear: Boolean?) {
