94981e8a26f5c46e440cb66fbbfcc77987d35ebf
igor.shaula <igor.shaula@gmail.com>
2024-02-06 03:29:59 +0200

    + FakeApiComponent and ContextComponent, also set dependencies between DI components

--- app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/data_sources/di/Components.kt
+++ app/src/main/kotlin/com/igor_shaula/api_polling/data_layer/data_sources/di/Components.kt
@@ -1,28 +1,44 @@
 package com.igor_shaula.api_polling.data_layer.data_sources.di
 
+import android.content.Context
+import com.igor_shaula.api_polling.data_layer.data_sources.fake_api_client.FakeVehicleGenerator
+import com.igor_shaula.api_polling.data_layer.data_sources.retrofit.VehiclesRetrofitNetworkService
 import com.igor_shaula.api_polling.ui_layer.SharedViewModel
 import dagger.Component
+import javax.inject.Singleton
+
+@Component(modules = [ContextModule::class])
+@Singleton
+interface ContextComponent {
+    fun getContext(): Context
+}
 
 @Component(
-//    dependencies = [FakeDSComponent::class, NetworkDSComponent::class],
-    modules = [RepositoryModule::class, FakeVehicleGeneratorModule::class, RetrofitModule::class, ContextModule::class]
+    dependencies = [FakeApiComponent::class, RetrofitComponent::class],
+    modules = [RepositoryModule::class /*FakeApiModule::class, RetrofitModule::class, ContextModule::class*/]
 )
 @RepositoryScope
 interface RepositoryComponent {
 
     fun inject(sharedViewModel: SharedViewModel)
-
-//    fun getVehiclesRepository(): VehiclesRepository // this instance is not provided for some reason
 }
 
 //@Component(dependencies = [RepositoryComponent::class])
 //@FakeDSScope
 //interface FakeDSComponent
-//
+
+@Component(/*dependencies = [RepositoryComponent::class], */modules = [FakeApiModule::class])
+@FakeApiScope
+interface FakeApiComponent {
+    fun getFakeVehicleGenerator(): FakeVehicleGenerator
+}
+
 //@Component(dependencies = [RepositoryComponent::class])
 //@NetworkDSScope
 //interface NetworkDSComponent
-//
-//@Component(dependencies = [NetworkDSComponent::class], modules = [RetrofitModule::class])
-//@RetrofitScope
-//interface RetrofitComponent
+
+@Component(/*dependencies = [RepositoryComponent::class], */modules = [RetrofitModule::class])
+@RetrofitScope
+interface RetrofitComponent {
+    fun getVehiclesRetrofitNetworkService(): VehiclesRetrofitNetworkService
+}
